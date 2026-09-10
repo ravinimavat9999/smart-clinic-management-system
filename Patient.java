a94570184bd14283a6565e0981320b841b4095c3
ravinimavat9999 <ravinimavat.mca@charusat.ac.in>
2026-09-10 22:06:35 +0530

    Create Patient.java

---
+++ Patient.java
@@ -0,0 +1,115 @@
+import java.time.LocalDate;
+
+public class Patient {
+
+    private Integer patientId;
+    private String name;
+    private String email;
+    private String password;
+    private String phone;
+    private LocalDate dateOfBirth;
+    private String gender;
+    private String address;
+    private String bloodGroup;
+    private String emergencyContact;
+
+    public Patient() {
+    }
+
+    public Patient(Integer patientId, String name, String email,
+                   String password, String phone, LocalDate dateOfBirth,
+                   String gender, String address, String bloodGroup,
+                   String emergencyContact) {
+
+        this.patientId = patientId;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.phone = phone;
+        this.dateOfBirth = dateOfBirth;
+        this.gender = gender;
+        this.address = address;
+        this.bloodGroup = bloodGroup;
+        this.emergencyContact = emergencyContact;
+    }
+
+    public Integer getPatientId() {
+        return patientId;
+    }
+
+    public void setPatientId(Integer patientId) {
+        this.patientId = patientId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getBloodGroup() {
+        return bloodGroup;
+    }
+
+    public void setBloodGroup(String bloodGroup) {
+        this.bloodGroup = bloodGroup;
+    }
+
+    public String getEmergencyContact() {
+        return emergencyContact;
+    }
+
+    public void setEmergencyContact(String emergencyContact) {
+        this.emergencyContact = emergencyContact;
+    }
+}
