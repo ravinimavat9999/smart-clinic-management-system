fa0a6ba5be9b98a8b4e7901947b7e3f3fe9f48f7
ravinimavat9999 <ravinimavat.mca@charusat.ac.in>
2026-09-10 22:12:18 +0530

    Create Prescription.java

---
+++ Prescription.java
@@ -0,0 +1,115 @@
+import java.time.LocalDate;
+
+public class Prescription {
+
+    private Integer prescriptionId;
+    private Integer patientId;
+    private Integer doctorId;
+    private Integer medicalRecordId;
+    private LocalDate prescriptionDate;
+    private String medicineName;
+    private String dosage;
+    private String frequency;
+    private String duration;
+    private String instructions;
+
+    public Prescription() {
+    }
+
+    public Prescription(Integer prescriptionId, Integer patientId, Integer doctorId,
+                        Integer medicalRecordId, LocalDate prescriptionDate,
+                        String medicineName, String dosage, String frequency,
+                        String duration, String instructions) {
+
+        this.prescriptionId = prescriptionId;
+        this.patientId = patientId;
+        this.doctorId = doctorId;
+        this.medicalRecordId = medicalRecordId;
+        this.prescriptionDate = prescriptionDate;
+        this.medicineName = medicineName;
+        this.dosage = dosage;
+        this.frequency = frequency;
+        this.duration = duration;
+        this.instructions = instructions;
+    }
+
+    public Integer getPrescriptionId() {
+        return prescriptionId;
+    }
+
+    public void setPrescriptionId(Integer prescriptionId) {
+        this.prescriptionId = prescriptionId;
+    }
+
+    public Integer getPatientId() {
+        return patientId;
+    }
+
+    public void setPatientId(Integer patientId) {
+        this.patientId = patientId;
+    }
+
+    public Integer getDoctorId() {
+        return doctorId;
+    }
+
+    public void setDoctorId(Integer doctorId) {
+        this.doctorId = doctorId;
+    }
+
+    public Integer getMedicalRecordId() {
+        return medicalRecordId;
+    }
+
+    public void setMedicalRecordId(Integer medicalRecordId) {
+        this.medicalRecordId = medicalRecordId;
+    }
+
+    public LocalDate getPrescriptionDate() {
+        return prescriptionDate;
+    }
+
+    public void setPrescriptionDate(LocalDate prescriptionDate) {
+        this.prescriptionDate = prescriptionDate;
+    }
+
+    public String getMedicineName() {
+        return medicineName;
+    }
+
+    public void setMedicineName(String medicineName) {
+        this.medicineName = medicineName;
+    }
+
+    public String getDosage() {
+        return dosage;
+    }
+
+    public void setDosage(String dosage) {
+        this.dosage = dosage;
+    }
+
+    public String getFrequency() {
+        return frequency;
+    }
+
+    public void setFrequency(String frequency) {
+        this.frequency = frequency;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+
+    public String getInstructions() {
+        return instructions;
+    }
+
+    public void setInstructions(String instructions) {
+        this.instructions = instructions;
+    }
+}
